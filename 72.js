28a27d87c1b44ee5aef1fe9741b3e47d35eeff42
Hemanth kumar lingabathina <buntytwo@proton.me>
2024-11-06 00:48:46 +0530

    Add files via upload

---
+++ 72.js
@@ -0,0 +1,15 @@
+// this is an answer to sudeer js 73/86
+
+let user1 = {
+name:"jacob",
+age : 28
+}
+let user2 = {
+name :"jacob",
+age : 28
+}
+console.log(user1===user2)
+
+
+//out put false 
+//javascript always compares by the memory location.
